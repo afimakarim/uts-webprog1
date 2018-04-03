c2c59cfa1a27fdb6e8d1bc078d8d228fc38f0226
afimakarim <36694376+afimakarim@users.noreply.github.com>
2018-04-03 18:49:13 +0700

    upload tugas uts
    upload tugas uts

---
+++ input.php
@@ -0,0 +1,19 @@
+<h2>Masukan Data</h2>
+<hr>
+<form action="simpan.php" method="post">
+    <table>
+        <tr>
+            <td>NIM </td>
+            <td>: <input type="text" name="nim"></td>
+        </tr>
+        <tr>
+            <td>NAMA</td>
+            <td> : <input type="text" name="nama"></td>
+        </tr>
+        <tr>
+            <td>JURUSAN</td>
+            <td> : <input type="text" name="jurusan"></td>
+        </tr>
+    </table>
+    <input type="submit" value="Simpan">
+</form>
---
+++ update.php
@@ -0,0 +1,21 @@
+<?php
+include "koneksi.php";
+$konkesiObj = new koneksi();
+$koneksi = $konkesiObj->getKoneksi();
+        
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$jurusan = $_POST['jurusan'];
+if($koneksi->connect_error) {
+    echo "Gagal Koneksi : " . $koneksi->connect_error;
+}
+$query = "update mahasiswa set nama='$nama', jurusan='$jurusan' where nim='$nim'";
+if($koneksi->query($query)===true){
+    echo "<br>Data ". $nama .' berhasil diupdate';
+} else{
+    echo "<br> Data gagal diupdate";
+    
+}
+echo "<br>";
+echo "<a href='index.php'>Kembali ke Halaman Utama</a>";
+?>
